bfc272bb1a738cf394ed85641f9f962842d81767
1001MJH <1001moon@hanafos.com>
2025-05-21 17:55:09 +0900

    1234

--- src/main/java/com/sist/web/DevopsApplication.java
+++ src/main/java/com/sist/web/DevopsApplication.java
@@ -2,7 +2,7 @@ package com.sist.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//***&&&&^^^^   
+//***&&&&^^^^ 1234  
 @SpringBootApplication
 public class DevopsApplication {
 
