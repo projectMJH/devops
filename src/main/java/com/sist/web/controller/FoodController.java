35301eff293b7cae2244046a8a3958195bd4a91c
1001MJH <1001moon@hanafos.com>
2025-06-13 17:16:28 +0900

    1111

--- src/main/java/com/sist/web/controller/FoodController.java
+++ src/main/java/com/sist/web/controller/FoodController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.sist.web.service.*;
@@ -57,6 +58,13 @@ public class FoodController {
 		model.addAttribute("vo",vo);
 		model.addAttribute("main_html","food/detail");
 		
+		return "index";
+	}
+	
+	@RequestMapping("/find")
+	public String food_find(Model model)
+	{
+		model.addAttribute("main_html","food/find");
 		return "index";
 	}
 }
